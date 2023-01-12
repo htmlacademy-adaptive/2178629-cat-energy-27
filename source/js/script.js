ff053267e500182ebb777beff263fe5d6d164a5e
BlindDenn <104034214+BlindDenn@users.noreply.github.com>
2023-01-12 17:34:32 +0300

    Настраивает карту iframe на index

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,10 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-header__nav-toggle');
+let addressMapContainer = document.querySelector('.section-address__map-container');
 
 navMain.classList.remove('main-nav--no-js');
 navToggle.classList.remove('main-header__nav-toggle--no-js');
+addressMapContainer.classList.remove('section-address__map-container--no-js');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
